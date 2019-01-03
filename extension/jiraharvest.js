3655a022b20ee225c9a09f5c1142837707fbf8d2
vdbongard <daniel@vdbongard.com>
2019-01-03 15:34:07 +0100

    Sidebar item gets set manually now if JIRA.API.getSidebar() is not supported.

--- extension/jiraharvest.js
+++ extension/jiraharvest.js
@@ -2,7 +2,7 @@ $(document).on('ready', function(){
 
   // pattern matching is quite broad so check if this is actually JIRA
   var appName = document.head.querySelector('[name=application-name]');
-  if(!appName || (appName && appName.content != 'JIRA')) {
+  if(!appName || (appName && appName.content !== 'JIRA')) {
     return;
   }
 
@@ -14,7 +14,7 @@ $(document).on('ready', function(){
     console.log('code injected');
 
     var $harvestTimer = $('#harvest-timer');
-    if ($harvestTimer.length == 0) {
+    if ($harvestTimer.length === 0) {
       $harvestTimer = $('<a id="harvest-timer" class="harvest-timer" href="#">Test</a>');
       $(document.body).append($harvestTimer);
 
@@ -58,7 +58,11 @@ $(document).on('ready', function(){
       $harvestTimer.get(0).click();
     };
 
+    let getSidebarResolved = false;
+
     JIRA.API.getSidebar().done((sb) => {
+      getSidebarResolved = true;
+
       var sidebarGroup = sb.getGroupAt(0);
       if (sb.addItem !== undefined) {
         var item = {
@@ -69,15 +73,7 @@ $(document).on('ready', function(){
           url: 'javascript:window.triggerHarvest(); void 0;'
         };
         sb.addItem(sidebarGroup.getId(), item, sidebarGroup.links.length);
-        var $iconSvg = $('#harvestTimer svg');
-        var $iconContainer = $iconSvg.parent();
-        $iconSvg.remove();
-
-        var $auiMedium = $('<style>.aui-icon-medium {height: 24px; width: 24px;} .aui-icon-medium:before {font-size: 20px; margin-top: -10px; margin-left: 2px;}</style>');
-        $iconContainer.append($auiMedium);
-
-        var $newIcon = $('<i class="aui-icon aui-icon-small aui-icon-medium aui-iconfont-time" />');
-        $iconContainer.append($newIcon);
+        setHarvestIcon(el);
       } else {
         var item = {
           iconClass: 'time',
@@ -90,6 +86,46 @@ $(document).on('ready', function(){
         sidebarGroup.$el.children().append($auiItem)
       }
     });
+
+    // Fallback for the case that JIRA.API.getSidebar() is not supported.
+    setTimeout(() => {
+      if (getSidebarResolved) {
+        return;
+      }
+
+      // The css classes are generated and may change. To find a sidebar item we are searching for the unique path of its svg icon.
+      const uniqueSidebarItemIdentifier = $('path[d=\'M5 15h12v-2H5v2zm-2-4h16v6H3v-6z\']');
+
+      // Finds the sidebar item root element
+      const sidebarItemRoot = uniqueSidebarItemIdentifier.closest('[role=presentation]:not(svg)');
+
+      const sidebarItem = sidebarItemRoot.clone();
+
+      const link = sidebarItem.find('a').first();
+
+      link.attr('href', 'javascript:window.triggerHarvest(); void 0;');
+      link.hover(
+          function () { $(this).css('background-color', 'rgb(235, 236, 240)') },
+          function () { $(this).css('background-color', 'transparent') }
+      );
+      link.find(':nth-child(2) :nth-child(1)').text('Track Time');
+
+      setHarvestIcon(link.find('svg'));
+
+      sidebarItemRoot.parent().append(sidebarItem);
+    }, 10);
+
+    function setHarvestIcon (el) {
+      var $iconSvg = el;
+      var $iconContainer = $iconSvg.parent();
+      $iconSvg.remove();
+
+      var $auiMedium = $('<style>.aui-icon-medium {height: 24px; width: 24px;} .aui-icon-medium:before {font-size: 20px; margin-top: -10px; margin-left: 2px;}</style>');
+      $iconContainer.append($auiMedium);
+
+      var $newIcon = $('<i class="aui-icon aui-icon-small aui-icon-medium aui-iconfont-time" />');
+      $iconContainer.append($newIcon);
+    }
   }
   // END INJECTION CODE
 
