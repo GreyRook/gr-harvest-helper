a68abaa2cfa4a633e5232542c2151a7dcd056eea
vdbongard <daniel@vdbongard.com>
2019-07-31 15:00:54 +0200

    Converted jQuery code to vanilla js

--- extension/jiraharvest.js
+++ extension/jiraharvest.js
@@ -1,27 +1,32 @@
-$(document).on('ready', function(){
+window.addEventListener( "load", init );
 
+function init(){
   // pattern matching is quite broad so check if this is actually JIRA
-  var appName = document.head.querySelector('[name=application-name]');
+  const appName = document.head.querySelector('[name=application-name]');
   if(!appName || (appName && appName.content !== 'JIRA')) {
     return;
   }
 
-  var $scriptHack = $('<script src=\'https://platform.harvestapp.com/assets/platform.js\'></script>');
-  $(document.head).append($scriptHack);
+  const script = document.createElement('script');
+  script.src = 'https://platform.harvestapp.com/assets/platform.js';
+
+  document.head.append(script);
 
   // BEGIN INJECTION CODE
-  var code = function() {
+  const code = function() {
     console.log('code injected');
 
-    var $harvestTimer = $('#harvest-timer');
-    if ($harvestTimer.length === 0) {
-      $harvestTimer = $('<a id="harvest-timer" class="harvest-timer" href="#">Test</a>');
-      $(document.body).append($harvestTimer);
+    function createElement(htmlString) {
+      const div = document.createElement('div');
+      div.innerHTML = htmlString.trim();
+      return div.firstChild;
+    }
 
-      $('#harvest-messaging').trigger({
-        type: 'harvest-event:timers:add',
-        element: $harvestTimer.get(0),
-      });
+    let harvestTimer =  document.getElementById('#harvest-timer');
+
+    if (!harvestTimer) {
+      harvestTimer = createElement('<a id="harvest-timer" class="harvest-timer" href="#">Test</a>');
+      document.body.append(harvestTimer);
     }
 
     window._harvestPlatformConfig = {
@@ -31,31 +36,34 @@ $(document).on('ready', function(){
     };
 
     window.triggerHarvest = function() {
-      var harvestGroup = {
+      const harvestGroup = {
         id: JIRA.API.Projects.getCurrentProjectKey(),
         name: JIRA.API.Projects.getCurrentProjectName(),
       };
-      $harvestTimer.attr('data-group', JSON.stringify(harvestGroup));
 
-      // request name from API
-      var issueTitle = null;
-      AJS.$.ajax({
-        url: "/rest/api/2/issue/" + JIRA.Issue.getIssueKey(),
-        type: 'get',
-        dataType: 'json',
-        async: false,
-        success: function(data) {
-          issueTitle = data.fields.summary;
-        },
-      });
+      harvestTimer.dataset.group = JSON.stringify(harvestGroup);
 
-      harvestItem = {
-        'id': JIRA.Issue.getIssueKey(),
-        'name': issueTitle,
-      };
-      $harvestTimer.attr('data-item', JSON.stringify(harvestItem));
-      // trigger native click
-      $harvestTimer.get(0).click();
+      let issueId = JIRA.Issue.getIssueKey();
+
+      if (!issueId) {
+        issueId = document.title.match(/\[(.*?)]/)[1];
+      }
+
+      fetch("/rest/api/2/issue/" + issueId, {headers: {'Content-Type': 'application/json'}})
+        .then(response => response.json())
+        .then(data => {
+          const issueTitle = data.fields.summary;
+
+          const harvestItem = {
+            'id': issueId,
+            'name': issueTitle,
+          };
+
+          harvestTimer.dataset.item = JSON.stringify(harvestItem);
+
+          // trigger native click
+          harvestTimer.click();
+        })
     };
 
     let getSidebarResolved = false;
@@ -63,9 +71,9 @@ $(document).on('ready', function(){
     JIRA.API.getSidebar().done((sb) => {
       getSidebarResolved = true;
 
-      var sidebarGroup = sb.getGroupAt(0);
+      const sidebarGroup = sb.getGroupAt(0);
       if (sb.addItem !== undefined) {
-        var item = {
+        const item = {
           id: 'harvestTimer',
           icon: 'time',
           name: 'Track Time',
@@ -73,17 +81,17 @@ $(document).on('ready', function(){
           url: 'javascript:window.triggerHarvest(); void 0;'
         };
         sb.addItem(sidebarGroup.getId(), item, sidebarGroup.links.length);
-        setHarvestIcon(el);
       } else {
-        var item = {
+        const item = {
           iconClass: 'time',
           isSelected: false,
           label: 'Track Time',
           href: 'javascript:window.triggerHarvest(); void 0;'
         }
 
-        var $auiItem = $(aui.navigation.item(item))
-        sidebarGroup.$el.children().append($auiItem)
+        const sidebarItem = aui.navigation.item(item);
+        const domItem = createElement(sidebarItem);
+        sidebarGroup.el.firstChild.append(domItem);
       }
     });
 
@@ -94,44 +102,43 @@ $(document).on('ready', function(){
       }
 
       // The css classes are generated and may change. To find a sidebar item we are searching for the unique path of its svg icon.
-      const uniqueSidebarItemIdentifier = $('path[d=\'M5 15h12v-2H5v2zm-2-4h16v6H3v-6z\']');
+      const uniqueSidebarItemIdentifier = document.querySelector('path[d=\'M5 15h12v-2H5v2zm-2-4h16v6H3v-6z\']');
 
       // Finds the sidebar item root element
       const sidebarItemRoot = uniqueSidebarItemIdentifier.closest('[role=presentation]:not(svg)');
 
-      const sidebarItem = sidebarItemRoot.clone();
+      const sidebarItem = sidebarItemRoot.cloneNode(true);
 
-      const link = sidebarItem.find('a').first();
+      const link = sidebarItem.querySelector('a');
 
-      link.attr('href', 'javascript:window.triggerHarvest(); void 0;');
-      link.hover(
-          function () { $(this).css('background-color', 'rgb(235, 236, 240)') },
-          function () { $(this).css('background-color', 'transparent') }
-      );
-      link.find(':nth-child(2) :nth-child(1)').text('Track Time');
+      link.href = 'javascript:window.triggerHarvest(); void 0;';
+      link.style.boxShadow = 'none'
+      link.onmouseenter = e => e.target.style.backgroundColor = 'rgb(235, 236, 240)'
+      link.onmouseleave = e => e.target.style.backgroundColor = ''
+      link.querySelector(':nth-child(2) :nth-child(1)').textContent = 'Track Time';
 
-      setHarvestIcon(link.find('svg'));
+      setHarvestIcon(link.querySelector('svg'));
 
-      sidebarItemRoot.parent().append(sidebarItem);
+      sidebarItemRoot.parentNode.append(sidebarItem);
     }, 10);
 
     function setHarvestIcon (el) {
-      var $iconSvg = el;
-      var $iconContainer = $iconSvg.parent();
-      $iconSvg.remove();
+      const iconSvg = el;
+      const iconContainer = iconSvg.parentNode;
+      iconSvg.remove();
 
-      var $auiMedium = $('<style>.aui-icon-medium {height: 24px; width: 24px;} .aui-icon-medium:before {font-size: 20px; margin-top: -10px; margin-left: 2px;}</style>');
-      $iconContainer.append($auiMedium);
+      const auiMedium = createElement('<style>.aui-icon-medium {height: 24px!important; width: 24px!important;} .aui-icon-medium:before {font-size: 20px; margin-top: -10px; margin-left: 2px;}</style>');
+      iconContainer.append(auiMedium);
 
-      var $newIcon = $('<i class="aui-icon aui-icon-small aui-icon-medium aui-iconfont-time" />');
-      $iconContainer.append($newIcon);
+      const newIcon = createElement('<i class="aui-icon aui-icon-small aui-icon-medium aui-iconfont-time" />');
+      iconContainer.append(newIcon);
     }
   }
   // END INJECTION CODE
 
-  var injection = `(${code.toString()})()`;
+  const injection = `(${code.toString()})()`;
 
-  var windowHack = document.createElement('script');
+  const windowHack = document.createElement('script');
   windowHack.textContent = injection;
   (document.head||document.documentElement).appendChild(windowHack);
-});
+}
