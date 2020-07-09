9471e56911f3cc7769f4afdc8aa9bccb47ab7c36
Beshoy Abdelmalak <b.abdelmalak@greyrook.com>
2020-07-09 11:55:27 +0200

    apply linting

--- popupTimer/popup.js
+++ popupTimer/popup.js
@@ -1,56 +1,54 @@
 window._harvestPlatformConfig = {
-  applicationName: "GreyRook",
-  skipStyling: true
+  applicationName: 'GreyRook',
+  skipStyling: true,
 };
 
 var taskName;
 var tabURL;
 var tab;
-chrome.runtime.onMessage.addListener(function(response) {
+chrome.runtime.onMessage.addListener(function (response) {
   taskName = response;
-  if (taskName == "") {
-    taskName = "select a task first";
+  if (taskName == '') {
+    taskName = 'select a task first';
   }
 });
 
-chrome.tabs.query({ currentWindow: true, active: true }, function(tab) {
+chrome.tabs.query({ currentWindow: true, active: true }, function (activeTab) {
   setTimeout(() => {
-    chrome.tabs.executeScript({ file: "/ticketName.js" });
+    chrome.tabs.executeScript({ file: '/ticketName.js' });
   }, 1000);
-  this.tab = tab;
+  tab = activeTab;
   tabURL = tab[0].url;
 });
 
-chrome.runtime.onInstalled.addListener(function(details) {
+chrome.runtime.onInstalled.addListener(function (details) {
   chrome.tabs.create({
-    url:"https://git.r0k.de/greyrook/foss/harvest-jira-chrome/blob/master/README.md"
+    url: 'https://git.r0k.de/greyrook/foss/harvest-jira-chrome/blob/master/README.md',
   });
   setTimeout(() => {
-    if (details.reason == "install") {
-      chrome.tabs.executeScript({ code: "installModal()" });
-    } else if (details.reason == "update") {
-      chrome.tabs.executeScript({ code: "updateModal()" });
+    if (details.reason == 'install') {
+      chrome.tabs.executeScript({ code: 'installModal()' });
+    } else if (details.reason == 'update') {
+      chrome.tabs.executeScript({ code: 'updateModal()' });
     }
   }, 1050);
 });
 
-window.onload = function() {
+window.onload = function () {
   let i = 0;
   const timeout = 2000; //2sec
   const intervalTime = 10;
   let taskNameInterval = setInterval(() => {
-    if (this.taskName !== undefined || i == timeout / intervalTime) {
+    if (taskName !== undefined || i == timeout / intervalTime) {
       clearInterval(taskNameInterval);
-      let item = { id: 1337, name: this.taskName };
-      const harvestTimer = this.document.getElementsByClassName(
-        "harvest-timer"
-      )[0];
-      harvestTimer.setAttribute("data-item", JSON.stringify(item));
-      if (this.taskName !== undefined) {
-        harvestTimer.setAttribute("data-permalink", this.tabURL);
+      let item = { id: 1337, name: taskName };
+      const harvestTimer = document.getElementsByClassName('harvest-timer')[0];
+      harvestTimer.setAttribute('data-item', JSON.stringify(item));
+      if (taskName !== undefined) {
+        harvestTimer.setAttribute('data-permalink', tabURL);
       }
       harvestTimer.click();
-      harvestTimer.setAttribute("top", "10px");
+      harvestTimer.setAttribute('top', '10px');
     } else {
       i++;
     }
@@ -60,18 +58,17 @@ window.onload = function() {
 var frameDetected = false;
 
 let detectFrame = setInterval(() => {
-  const harvestIframe = document.getElementById("harvest-iframe");
+  const harvestIframe = document.getElementById('harvest-iframe');
   if (harvestIframe && !frameDetected) {
+    clearInterval(detectFrame);
     frameDetected = true;
 
-    harvestIframe.style.top = "10px";
+    harvestIframe.style.top = '10px';
 
-    const harvestOverlay = this.document.getElementsByClassName(
-      "harvest-overlay"
-    )[0];
+    const harvestOverlay = this.document.getElementsByClassName('harvest-overlay')[0];
 
-    harvestOverlay.style.background = "white";
-    harvestOverlay.style.overflow = "hidden";
+    harvestOverlay.style.background = 'white';
+    harvestOverlay.style.overflow = 'hidden';
 
     let scrollHeight = 300;
     setInterval(() => {
@@ -80,8 +77,8 @@ let detectFrame = setInterval(() => {
         harvestIframe.scrollHeight !== 0 &&
         harvestIframe.scrollHeight === scrollHeight
       ) {
-        document.body.style.height = harvestIframe.scrollHeight + "px";
-        document.body.style.width = "500px";
+        document.body.style.height = harvestIframe.scrollHeight + 'px';
+        document.body.style.width = '500px';
       } else {
         scrollHeight = harvestIframe.scrollHeight;
       }
--- ticketName.js
+++ ticketName.js
@@ -1,77 +1,76 @@
+/* eslint-disable no-unused-vars */
 function updateModal() {
   modal(`<p> Welcome to the new version of Harvest Plugin! </p> 
   <p> The way this plugin works has changed a bit: </p> 
   <p> Select an issue in Jira, Zammad or GitLab and then click on the extension button in the upper right corner to start tracking time in Harvest.</p>`);
 }
+
 function installModal() {
   modal(`<p>Welcome to Harvest Plugin!</p>
   <p> Select an issue in Jira, Zammad or GitLab and then click on the extension button in the upper right corner to start tracking time in Harvest.</p>`);
 }
-function modal(text) {
-  const modal = document.createElement("dialog");
-  modal.innerHTML = text;
-  modal.style.zIndex = "1500";
-  modal.style.maxWidth = "500px";
-  modal.style.height = "min-content";
-  modal.style.top = "5%";
-  modal.style.display = "inline-flex";
-  modal.style.flexDirection = "column";
-  modal.style.borderColor = "#f36c00";
-  modal.style.borderRadius = "10px";
-  modal.style.fontSize = "18px";
+/* eslint-enable no-unused-vars */
 
-  const modalImg = document.createElement("img");
-  modalImg.src = chrome.extension.getURL("images/modalImage.png");
-  modalImg.style.marginTop = "10px";
-  modalImg.style.width = "100%";
-  modalImg.style.height = "auto";
+function modal(text) {
+  const modal = document.createElement('dialog');
+  modal.innerHTML = text;
+  modal.style.zIndex = '1500';
+  modal.style.maxWidth = '500px';
+  modal.style.height = 'min-content';
+  modal.style.top = '5%';
+  modal.style.display = 'inline-flex';
+  modal.style.flexDirection = 'column';
+  modal.style.borderColor = '#f36c00';
+  modal.style.borderRadius = '10px';
+  modal.style.fontSize = '18px';
+
+  const modalImg = document.createElement('img');
+  modalImg.src = chrome.extension.getURL('images/modalImage.png');
+  modalImg.style.marginTop = '10px';
+  modalImg.style.width = '100%';
+  modalImg.style.height = 'auto';
   modal.appendChild(modalImg);
 
-  const closeButton = document.createElement("button");
-  closeButton.style.background = "#4fb840";
-  closeButton.style.color = "#fff";
-  closeButton.style.fontWeight = "600";
-  closeButton.style.fontSize = "16px";
-  closeButton.style.lineHeight = "38px";
-  closeButton.style.borderRadius = "4px";
-  closeButton.style.marginTop = "10px";
-  closeButton.style.borderStyle = "solid";
-  closeButton.style.borderColor = "darkgreen";
-  closeButton.style.borderWidth = "thin";
-  closeButton.innerHTML = "OK";
+  const closeButton = document.createElement('button');
+  closeButton.style.background = '#4fb840';
+  closeButton.style.color = '#fff';
+  closeButton.style.fontWeight = '600';
+  closeButton.style.fontSize = '16px';
+  closeButton.style.lineHeight = '38px';
+  closeButton.style.borderRadius = '4px';
+  closeButton.style.marginTop = '10px';
+  closeButton.style.borderStyle = 'solid';
+  closeButton.style.borderColor = 'darkgreen';
+  closeButton.style.borderWidth = 'thin';
+  closeButton.innerHTML = 'OK';
   closeButton.onclick = () => {
-    modal.style.display = "none";
+    modal.style.display = 'none';
     modal.close();
   };
   modal.appendChild(closeButton);
- 
+
   document.body.append(modal);
 }
 
 function GRlog(text) {
-  console.log("[GR-Time-Tracker]: " + text);
+  console.log('[GR-Time-Tracker]: ' + text);
 }
 
 function detectJira() {
-  const appName = document.querySelector("[name=application-name]");
-  return appName && appName.content == "JIRA";
+  const appName = document.querySelector('[name=application-name]');
+  return appName && appName.content == 'JIRA';
 }
 
 function detectZammad() {
   return (
-    document &&
-    document.getElementsByClassName(
-      "ticket-title-update js-objectTitle"
-    ).length > 0
+    document && document.getElementsByClassName('ticket-title-update js-objectTitle').length > 0
   );
 }
 
 function detectGitlab() {
   return (
     document &&
-    document.getElementsByClassName(
-      "detail-page-header-actions js-issuable-actions"
-    ).length > 0
+    document.getElementsByClassName('detail-page-header-actions js-issuable-actions').length > 0
   );
 }
 
@@ -79,50 +78,43 @@ async function jiraGetIssueTitle() {
   let issueId;
   try {
     issueId = document.title.match(/\[(.*?)]/)[1];
-  } catch {
+  } catch (error) {
     const urlParams = new URLSearchParams(window.location.search);
-    issueId = urlParams.get("selectedIssue");
+    issueId = urlParams.get('selectedIssue');
   }
-  if (!issueId) return "";
+  if (!issueId) return '';
 
   const protocol = window.location.protocol;
   const hostname = window.location.hostname;
-  const issueTitle = fetch(
-    protocol + "//" + hostname + "/rest/api/2/issue/" + issueId,
-    {
-      headers: { "Content-Type": "application/json" }
-    }
-  )
-    .then(response => response.json())
-    .then(data => {
+  const issueTitle = fetch(protocol + '//' + hostname + '/rest/api/2/issue/' + issueId, {
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then((response) => response.json())
+    .then((data) => {
       return data.fields.summary;
     });
   return issueTitle;
 }
 
 function zammadGetIssueTitle() {
-  return document.getElementsByClassName(
-    "ticket-title-update js-objectTitle"
-  )[0].textContent;
+  return document.getElementsByClassName('ticket-title-update js-objectTitle')[0].textContent;
 }
 
 function gitlabGetIssueTitle() {
-  var taskName = document.getElementsByClassName("title qa-title")[0]
-    .textContent;
-  var taskId = document.getElementsByClassName("breadcrumbs-sub-title")[0]
-    .textContent;
-  return taskName + "(" + taskId + ")";
+  var taskName = document.getElementsByClassName('title qa-title')[0].textContent;
+  var taskId = document.getElementsByClassName('breadcrumbs-sub-title')[0].textContent;
+  return taskName + '(' + taskId + ')';
 }
 
 if (detectJira()) {
-  GRlog("jira detected");
-  jiraGetIssueTitle().then(res => {
+  GRlog('jira detected');
+  jiraGetIssueTitle().then((res) => {
     chrome.runtime.sendMessage(res);
   });
 } else if (detectZammad()) {
-  GRlog("zammad detected");
+  GRlog('zammad detected');
   chrome.runtime.sendMessage(zammadGetIssueTitle());
 } else if (detectGitlab()) {
-  GRlog("gitlab detected");
+  GRlog('gitlab detected');
   chrome.runtime.sendMessage(gitlabGetIssueTitle());
 }
