ac134ea50d0873a0116b3e07a8d11e47e9b6b610
Beshoy Abdelmalak <b.abdelmalak@greyrook.com>
2020-07-09 12:28:04 +0200

    fix indentation in .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
-module.exports =  {
-    semi:  true,
-    trailingComma:  'es5',
-    singleQuote:  true,
-    printWidth:  100,
-    tabWidth:  2,
+module.exports = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
 };
